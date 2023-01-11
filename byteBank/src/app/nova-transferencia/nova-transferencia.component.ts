310b113a854f4f2e3e1a088b43face4671ba40d6
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-10 23:24:12 -0300

    Property Bind

--- byteBank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ byteBank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+  valor: number;
+  destino: number;
+
   transferir(){
     console.log('Solicitada nova transferencia');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
   }
 
 }
