8720346616b6b7bc02ced3307984e3e7c02bd1f4
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-10 23:15:38 -0300

    Criando evento do formulario

--- byteBank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ byteBank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -5,11 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss']
 })
-export class NovaTransferenciaComponent implements OnInit {
+export class NovaTransferenciaComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  transferir(){
+    console.log('Solicitada nova transferencia');
   }
 
 }
