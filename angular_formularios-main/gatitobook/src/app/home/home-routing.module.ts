66cac8166cb65cba0e157bcc66b15e3b163b6325
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-13 19:31:55 -0300

    Componente Login

--- angular_formularios-main/gatitobook/src/app/home/home-routing.module.ts
+++ angular_formularios-main/gatitobook/src/app/home/home-routing.module.ts
@@ -1,12 +1,19 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:'',
-    component:HomeComponent
-  }
+    path: '',
+    component:HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
