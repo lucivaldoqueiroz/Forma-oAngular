18cbd4c3d9fb0763a3295f47a9035a66747f2e18
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-14 15:22:48 -0300

    Redirecionamento Rota animais

--- angular_formularios-main/gatitobook/src/app/home/login/login.component.ts
+++ angular_formularios-main/gatitobook/src/app/home/login/login.component.ts
@@ -1,5 +1,6 @@
 import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,13 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor(private authService: AutenticacaoService) { }
+  constructor(private authService: AutenticacaoService, private router:Router) { }
 
   ngOnInit(): void {}
 
   login() {
     this.authService.autentica(this.usuario, this.senha).subscribe(() => {
-      console.log('Autenticado com sucesso');
+     this.router.navigate(['animais']);
     },(error) => {
       alert("Usuario ou senha invalido");
       console.log(error);
